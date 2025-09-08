e3dbb6ee49abc82103d7f8c79c4dc3b85aee08ce
Mykhailo Shapovalov <65449116+Kotorama@users.noreply.github.com>
2025-09-08 11:18:11 +0300

    added a comment to Task 2

--- Task 2/untitled/src/main/java/org/example/Main.java
+++ Task 2/untitled/src/main/java/org/example/Main.java
@@ -1,5 +1,13 @@
 package org.example;
 
+//V 10: Character Operations
+//Objective: To understand and use primitive char type in Java.
+//Instructions:
+//Declare a char variable.
+//Use Character methods (e.g., isDigit, isLetter, toUpperCase, toLowerCase) to manipulate the char.
+//Print the result of each operation to the console.
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
